bae4c0d497fe41cd9f0a4f118d2d01f04b910831
sebas7532silva <sebassilva7532@hotmail.com>
2020-05-24 19:16:09 -0500

    commit

--- node.js
+++ node.js
@@ -1,2 +1,14 @@
+var express = require('express');
+app = express();
 
-console.log("Hello world!\n");
+app.get('/', function(req, res){
+    res.send('Hello World!\n');
+});
+
+app.get('/', function(req, res){
+    res.send('Hello Mars!\n');
+});
+
+app.listen(8080, function(){
+    console.log('Example app listening on port 8080!');
+});
